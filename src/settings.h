22a121399a8e6dacaf9071357a1c1b756fb3c680
Quentin Guidée <quentin.guidee@gmail.com>
2020-12-17 16:03:08 +0100

    [settings] Comment pin type

--- src/settings.h
+++ src/settings.h
@@ -7,21 +7,25 @@
 
 /**
  * ESC Motor A Pin (front left)
+ * Pin type: D~
  */
 constexpr uint8_t ESC_MOTOR_A_PIN = 6;
 
 /**
  * ESC Motor B Pin (front right)
+ * Pin type: D~
  */
 constexpr uint8_t ESC_MOTOR_B_PIN = 5;
 
 /**
  * ESC Motor C Pin (rear left)
+ * Pin type: D~
  */
 constexpr uint8_t ESC_MOTOR_C_PIN = 10;
 
 /**
  * ESC Motor D Pin (rear right)
+ * Pin type: D~
  */
 constexpr uint8_t ESC_MOTOR_D_PIN = 11;
 
@@ -41,21 +45,25 @@ constexpr int16_t MAX_ESC_PULSE_WIDTH = 2000;
 
 /**
  * LED Motor A Pin (front left)
+ * Pin type: D
  */
 constexpr uint8_t LED_MOTOR_A_PIN = 2;
 
 /**
  * LED Motor B Pin (front right)
+ * Pin type: D
  */
 constexpr uint8_t LED_MOTOR_B_PIN = 7;
 
 /**
  * LED Motor C Pin (rear left)
+ * Pin type: D
  */
 constexpr uint8_t LED_MOTOR_C_PIN = 8;
 
 /**
  * LED Motor D Pin (rear right)
+ * Pin type: D
  */
 constexpr uint8_t LED_MOTOR_D_PIN = 12;
 
@@ -63,6 +71,10 @@ constexpr uint8_t LED_MOTOR_D_PIN = 12;
 // === PIEZO === //
 ///////////////////
 
+/**
+ * Piezo Pin
+ * Pin type: D
+ */
 constexpr uint8_t PIEZO_PIN = 4;
 
 #endif /* SETTINGS_H */
