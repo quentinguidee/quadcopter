b711d9b89dad81a3c4204a1090156f48de299e61
Quentin Guidée <quentin.guidee@gmail.com>
2020-12-18 19:53:25 +0100

    [settings] Change Arduino Uno pins to Arduino Mega pins

--- src/settings.h
+++ src/settings.h
@@ -9,25 +9,25 @@
  * ESC Motor A Pin (front left)
  * Pin type: D~
  */
-constexpr uint8_t ESC_MOTOR_A_PIN = 6;
+constexpr uint8_t ESC_MOTOR_A_PIN = 2;
 
 /**
  * ESC Motor B Pin (front right)
  * Pin type: D~
  */
-constexpr uint8_t ESC_MOTOR_B_PIN = 5;
+constexpr uint8_t ESC_MOTOR_B_PIN = 3;
 
 /**
  * ESC Motor C Pin (rear left)
  * Pin type: D~
  */
-constexpr uint8_t ESC_MOTOR_C_PIN = 10;
+constexpr uint8_t ESC_MOTOR_C_PIN = 4;
 
 /**
  * ESC Motor D Pin (rear right)
  * Pin type: D~
  */
-constexpr uint8_t ESC_MOTOR_D_PIN = 11;
+constexpr uint8_t ESC_MOTOR_D_PIN = 5;
 
 /**
  * Pulse width corresponding to 0°
@@ -47,25 +47,25 @@ constexpr int16_t MAX_ESC_PULSE_WIDTH = 2000;
  * LED Motor A Pin (front left)
  * Pin type: D
  */
-constexpr uint8_t LED_MOTOR_A_PIN = 2;
+constexpr uint8_t LED_MOTOR_A_PIN = 22;
 
 /**
  * LED Motor B Pin (front right)
  * Pin type: D
  */
-constexpr uint8_t LED_MOTOR_B_PIN = 7;
+constexpr uint8_t LED_MOTOR_B_PIN = 23;
 
 /**
  * LED Motor C Pin (rear left)
  * Pin type: D
  */
-constexpr uint8_t LED_MOTOR_C_PIN = 8;
+constexpr uint8_t LED_MOTOR_C_PIN = 24;
 
 /**
  * LED Motor D Pin (rear right)
  * Pin type: D
  */
-constexpr uint8_t LED_MOTOR_D_PIN = 12;
+constexpr uint8_t LED_MOTOR_D_PIN = 25;
 
 ///////////////////
 // === PIEZO === //
@@ -75,7 +75,7 @@ constexpr uint8_t LED_MOTOR_D_PIN = 12;
  * Piezo Pin
  * Pin type: D
  */
-constexpr uint8_t PIEZO_PIN = 4;
+constexpr uint8_t PIEZO_PIN = 53;
 
 /**
  * Number of frequencies for the startup ring.
